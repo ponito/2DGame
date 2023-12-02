a3f5544e0e9e9b27c3e4756ac1ee86d6367800d2
ponito <78309031+ponito@users.noreply.github.com>
2023-12-02 14:20:40 +0100

    changes

--- Player/player.gd
+++ Player/player.gd
@@ -14,15 +14,25 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		if is_on_wall() and Input.is_action_pressed("Left"):
+			velocity.x = SPEED
+			velocity.y = JUMP_VELOCITY
+		if is_on_wall() and Input.is_action_pressed("Right"):
+			velocity.x = -SPEED
+			velocity.y = JUMP_VELOCITY
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("Left", "Right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if Input.is_action_pressed("Right"):
+		velocity.x = min(velocity.x + SPEED * delta * 4, SPEED)
+	elif Input.is_action_pressed("Left"):
+			velocity.x = max(velocity.x - SPEED * delta * 4, -SPEED)
+	elif is_on_floor():
+		velocity.x = lerp(velocity.x, 0., 0.2)
 
 	move_and_slide()
