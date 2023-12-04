a076411e9cf90cb1b3eed3bd489e9034608ff754
LuxbraeTheMage <152695270+LuxbraeTheMage@users.noreply.github.com>
2023-12-04 17:35:51 +0100

    c

---
+++ Global/master.gd
@@ -0,0 +1,3 @@
+extends Node
+var playerHealth = 0
+var playerKnowledge = 0
---
+++ Global/save.gd
@@ -0,0 +1,25 @@
+extends Node
+
+
+const SAVE_PATH  = "res://savegame.bin"
+
+func saveGame():
+	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+	var data: Dictionary = {
+		"playerHealth": master.playerHealth,
+		"playerKnowledge": master.playerKnowledge
+		
+	}
+	var jstr = JSON.stringify(data)
+	file.store_line(jstr)
+
+
+func loadGame():
+	var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
+	if FileAccess.file_exists(SAVE_PATH) == true:
+		if not file.eof_reached():
+			var curren_line = JSON.parse_string(file.get_line())
+			if curren_line:
+				master.playerHealth = curren_line["playerHealth"]
+				master.playerKnowledge = curren_line["playerKnowledge"]
+
