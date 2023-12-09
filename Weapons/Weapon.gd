9386d5857f2ec9b890a03325dcd95989ec1ec5dc
LuxbraeTheMage <152695270+LuxbraeTheMage@users.noreply.github.com>
2023-12-09 14:40:07 +0100

    c

--- Weapons/Weapon.gd
+++ Weapons/Weapon.gd
@@ -18,9 +18,11 @@ func _process(delta):
 		#$activeWeaponEffect.queue_free()
 
 
+
+
+
 func _on_animation_player_animation_finished(anim_name):
+	print("Hi")
 	player.Ocupied = 0
 	$activeWeaponEffect.queue_free()
-	#if activeWeaponEffect != null:
-		
 	pass # Replace with function body.
