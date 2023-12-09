98d2d707b3d46052552fe7a89235edc1d4412ad5
LuxbraeTheMage <152695270+LuxbraeTheMage@users.noreply.github.com>
2023-12-09 15:04:44 +0100

    c

--- Weapons/WeaponEffect.gd
+++ Weapons/WeaponEffect.gd
@@ -4,4 +4,6 @@ var EnemyHyperArmor = 0
 
 func _on_area_2d_body_entered(body):
 	body.Health -= 20
+	
 	EnemyHyperArmor = EnemyHyperArmor + body.EnemyHyperArmor
+	body.velocity.x += sign(direction.x) * 500
