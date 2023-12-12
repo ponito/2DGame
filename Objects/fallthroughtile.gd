d0e95e13e010894da65541132d22ff541b22f42c
LuxbraeTheMage <152695270+LuxbraeTheMage@users.noreply.github.com>
2023-12-12 11:42:18 +0100

    c

--- Objects/fallthroughtile.gd
+++ Objects/fallthroughtile.gd
@@ -1,20 +1,19 @@
 extends Node2D
-var collisionx = false
+var collisionx
 
-@onready var player = get_node("../../player/player")
+@onready var player = get_node("../player/player")
 
 func  _process(delta):
 	# Handle Fallthrough Platforms fallthrough.
 	if Input.is_action_just_pressed("S") && collisionx == true:
 		player.position.y += 1
-		pass
+
 
 
 
 
 
 func _on_area_2d_body_entered(body):
-
 	if body == player:
 		collisionx = true
 
--- Player/player.gd
+++ Player/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 360.0
-const JUMP_VELOCITY = -500.0
+const JUMP_VELOCITY = -550.0
 var Health = master.playerHealth:
 	get:
 		return Health
